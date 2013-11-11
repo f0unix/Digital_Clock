cac505ddc6c2730c94dc162c737555f5eeeb0584
yuri <yurigh00@gmail.com>
2013-11-12 00:37:25 +0200

    Numerous

--- js/time.js
+++ js/time.js
@@ -10,16 +10,44 @@ var view = {
   _8:[2,3,4,6,10,11,15,17,18,19,21,25,26,30,32,33,34],
   _9:[2,3,4,6,10,11,15,17,18,19,20,25,26,30,32,33,34]
 };   
-
-
-
 $(document).ready(function() {
   function myDate(){
     var now = new Date();
     var outHour = now.getHours();
     var outMin = now.getMinutes();
     var outSec = now.getSeconds();
-    
+    outHour+='';
+    if(outHour.length<2){
+      var time;
+      switch(outHour){
+        case '0':time=view._0;
+        break;
+        case '1':time=view._1;
+        break;
+        case '2':time=view._2;
+        break;
+        case '3':time=view._3;
+        break;
+        case '4':time=view._4;
+        break;
+        case '5':time=view._5;
+        break;
+        case '6':time=view._6;
+        break;
+        case '7':time=view._7;
+        break;
+        case '8':time=view._8;
+        break;
+        case '9':time=view._9;
+        break;
+        default:console.log('Couldn\'t detect hours');break;
+      }
+      console.log(time);
+    }
+    //$("#secs_bit1 ul li div.secs "+i+" box")
+    //$("#secs_bit2 ul li div.secs "+i+" box")
+    //console.log(outHour+":"+outMin+":"+outSec);
+
   } 
   myDate();
   setInterval(function(){myDate();}, 1000);
