c032587ef8e7b392472d14bf9ff1135a273c3218
yuri <yuri@yuri.(none)>
2013-11-11 14:55:32 +0200

    NumNumNum

--- js/time.js
+++ js/time.js
@@ -1,11 +1,11 @@
-  $(document).ready(function() {
+ $(document).ready(function() {
   function myDate(){
-  var now = new Date();
-  var outHour = now.getHours();
-  var outMin = now.getMinutes();
-  var outSec = now.getSeconds();
+    var now = new Date();
+    var outHour = now.getHours();
+    var outMin = now.getMinutes();
+    var outSec = now.getSeconds();
+    console.log(outHour+' : '+outMin+' : '+outSec);
   } 
   myDate();
   setInterval(function(){   myDate();}, 1000);
-
-  });
+});
