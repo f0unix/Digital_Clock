6b19caac565669ee7004221608964fca3c51e31f
yuri <yurigh00@gmail.com>
2013-11-12 21:39:07 +0200

    Ca marche bien

--- js/time.js
+++ js/time.js
@@ -17,37 +17,9 @@ $(document).ready(function() {
     var outMin = now.getMinutes();
     var outSec = now.getSeconds();
     outSec+='';
-   
-    if(outSec.length<2){
-      var time = getMatrix(outSec);
-      $("#secs_bit1 ul li div.secs").removeClass('orange');
-      $("#secs_bit2 ul li div.secs").removeClass('orange');
-      for(i=0;i<view._0.length;i++){
-        $("#secs_bit1 ul li div.secs."+view._0[i]+".box").addClass('orange');
-      }
-      for(i=0;i<time.length;i++){
-        $("#secs_bit2 ul li div.secs."+time[i]+".box").addClass('orange');
-      }
-
-    }
-    else {var bit1 = outSec.substring(0,1);
-      var bit2 = outSec.substring(1,2);
-      // returns an array 
-      var time1 = getMatrix(bit1);
-      var time2 = getMatrix(bit2);
-      $("#secs_bit1 ul li div.secs").removeClass('orange');
-      for(i=0;i<time1.length;i++){
-        $("#secs_bit1 ul li div.secs."+time1[i]+".box").addClass('orange');
-      }
-      $("#secs_bit2 ul li div.secs").removeClass('orange');
-
-
-      for(i=0;i<time2.length;i++){
-        $("#secs_bit2 ul li div.secs."+time2[i]+".box").addClass('orange');
-      }
-    }
-
-
+    Display("hours",outHour);
+    Display("mins",outMin);
+    Display("secs",outSec);
   } 
   myDate();
   setInterval(function(){myDate();}, 1000);
